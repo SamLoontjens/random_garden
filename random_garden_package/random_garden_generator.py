1730d71e220495340c91fe39d286f0a5ac541bc4
SamLoontjens <93867680+SamLoontjens@users.noreply.github.com>
2023-12-27 05:32:17 +0100

    implemented biomes and their weathers

--- random_garden_package/random_garden_generator.py
+++ random_garden_package/random_garden_generator.py
@@ -1,20 +1,38 @@
 import random
 import time
+import datetime
 import pkg_resources
 import os
 
 # define ascii art class
 class AsciiArt:
-  # Define available weathers and biomes
-  available_weathers = {'day', 'night', 'rain', 'snow'}
-  available_biomes = {'ice', 'tundra', 'taiga', 'temperate', 'grassland', 'desert', 'steppe', 'savanna', 'tropical', 'swamp', 'mangrove', 'lake', 'ocean'}
+  # biomes and their corresponding weathers
+  biomes_and_weathers = {
+    'ice': {'day', 'night', 'snow'},
+    'tundra': {'day', 'night', 'rain', 'snow'},
+    'taiga': {'day', 'night', 'rain', 'snow'},
+    'temperate': {'day', 'night', 'rain', 'snow'},
+    'grassland': {'day', 'night', 'rain', 'snow'},
+    'desert': {'day', 'night'}, 
+    'steppe': {'day', 'night', 'rain'},
+    'savanna': {'day', 'night', 'rain'},
+    'tropical': {'day', 'night', 'rain',},
+    'swamp': {'day', 'night', 'rain'},
+    'mangrove': {'day', 'night', 'rain'},
+    'lake': {'day', 'night', 'rain', 'snow'},
+    'ocean': {'day', 'night', 'rain', 'snow'},
+    'underwater': {'day', 'night', 'rain', 'snow'},
+    'space': {'night'},
+    'fantasy': {'day', 'night', 'rain', 'snow'},
+    }
 
-  def __init__(self, name, category, weather, biome, rarity, artists, editors, source, original_art, new_art):
+  def __init__(self, name, category, weather, biome, rarity, dates, artists, editors, source, original_art, new_art):
     self.name = name
     self.category = category
     self.weather = weather
     self.biome = biome
     self.rarity = rarity
+    self.dates = dates
     self.artists = artists if artists else []
     self.editors = editors if editors else []
     self.source = source
@@ -34,6 +52,7 @@ class AsciiArt:
     artists = []
     editors = []
     rarity = 1
+    dates = []
 
     original_art_lines = []
     new_art_lines = []
@@ -43,7 +62,7 @@ class AsciiArt:
       line = line.rstrip('\n')
 
       if line.startswith('[') and line.endswith(']'):
-        current_section = line[1:-1].lower()
+        current_section = line.strip().lower()[1:-1]
         is_new_art = current_section == 'new'
         continue
 
@@ -57,6 +76,8 @@ class AsciiArt:
         biome = line.strip().split(', ') if line.strip() else []
       elif current_section == 'rarity':
         rarity = int(line.strip())
+      elif current_section == 'dates':
+        dates = [d.strip() for d in line.split(',')] if line.strip() else []
       elif current_section == 'artists':
         artists = [a.strip() for a in line.split(',')] if line.strip() else []
       elif current_section == 'editors':
@@ -71,7 +92,7 @@ class AsciiArt:
     original_art = ''.join(original_art_lines)
     new_art = ''.join(new_art_lines)
 
-    return AsciiArt(name, category, weather, biome, rarity, artists, editors, source, original_art, new_art)
+    return AsciiArt(name, category, weather, biome, rarity, dates, artists, editors, source, original_art, new_art)
 
   def _split_art_and_get_dimensions(self):
     # Split new art into lines or if there is no new art split original art into lines
@@ -97,15 +118,16 @@ class AsciiArt:
     return art_lines, height, width
 
   def validate_attributes(self):
-    # Validate weather
-    for w in self.weather:
-      if w not in self.available_weathers:
-        raise ValueError(f"Invalid weather '{w}' in art '{self.name}'. Valid options: {', '.join(self.available_weathers)}")
-
-    # Validate biome
+    # Ensure all listed biomes are valid
     for b in self.biome:
-      if b not in self.available_biomes:
-        raise ValueError(f"Invalid biome '{b}' in art '{self.name}'. Valid options: {', '.join(self.available_biomes)}")
+      if b not in self.biomes_and_weathers:
+        raise ValueError(f"Invalid biome '{b}' in art '{self.name}'. Valid biomes: {', '.join(self.biomes_and_weathers.keys())}")
+
+    # Check if each weather condition is valid in at least one of the biomes
+    for w in self.weather:
+      if not any(w in self.biomes_and_weathers[b] for b in self.biome):
+        valid_biomes_for_weather = [b for b in self.biome if w in self.biomes_and_weathers[b]]
+        raise ValueError(f"Weather '{w}' is not typical for the biomes '{', '.join(self.biome)}' in art '{self.name}'. Valid biomes for this weather: {', '.join(valid_biomes_for_weather)}")
 
   def __str__(self):
     return self.new_art if self.new_art else self.original_art
@@ -114,6 +136,7 @@ class AsciiArt:
 def load_ascii_art(folder_name='art/flowers/'):
   art_list = []
   folder_path = pkg_resources.resource_filename('random_garden_package', folder_name)
+  today = datetime.datetime.now().strftime("%m:%d")  # Current date in mm:dd format
 
   if not os.path.exists(folder_path):
     return art_list
@@ -122,9 +145,35 @@ def load_ascii_art(folder_name='art/flowers/'):
     if filename.endswith('.txt'):
       file_path = os.path.join(folder_path, filename)
       art = AsciiArt.load_from_file(file_path)
-      art_list.append(art)
+      # Check if today matches on of the dates or if there's no date specified
+      if not art.dates or today in art.dates:
+        art_list.append(art)
 
   return art_list
+
+def concatenate_art_by_height():
+  all_art = load_ascii_art('art/flowers/') + load_ascii_art('art/animals/') + load_ascii_art('art/buildings/')
+
+  # Sort the art by height
+  sorted_art = sorted(all_art, key=lambda art: art.height)
+
+  # Find the maximum height among all art
+  max_height = max(art.height for art in sorted_art) if sorted_art else 0
+
+  # Concatenate art side by side with alignment at bottom
+  concatenated_art_lines = [''] * max_height
+  for art in sorted_art:
+    space_above = max_height - art.height  # Calculate space needed above the art
+    for i in range(max_height):
+      if i < space_above:
+        # Add spaces for lines above the current art
+        art_line = ' ' * art.width
+      else:
+        # Add the actual art line
+        art_line = art.art_lines[i - space_above]
+      concatenated_art_lines[i] += art_line + ' '  # Add a space as a separator
+
+  return '\n'.join(concatenated_art_lines)
 
 # Generates a random garden
 def random_garden(seed = None,
@@ -144,6 +193,11 @@ def random_garden(seed = None,
   print(f"\nThe selected draw height is:       {draw_height}") if info >= 2 else None
   print(f"The selected draw width is:        {draw_width}\n") if info >= 2 else None
 
+  # Check if draw height is enough for underwater otherwise change to ocean
+  if biome == 'underwater' and draw_height < 22:
+    biome = 'ocean'
+    print(f"Adjusted biome to 'ocean' because draw_height was under 22.") if info >= 3 else None
+
   # select seed
   if seed is None:
     seed = round(time.time())
@@ -161,12 +215,18 @@ def random_garden(seed = None,
   if weather not in weather_chars_weights:
     raise ValueError(f"Invalid weather type: {weather}. Valid options are: {', '.join(weather_chars_weights.keys())}")
 
-  biomes = ('ice', 'tundra', 'taiga', 
-            'temperate', 'grassland', 'desert', 
-            'steppe', 'savanna', 'tropical', 
-            'swamp', 'mangrove', 'lake', 'ocean')
-  if biome not in biomes:
-    raise ValueError(f"Invalid weather type: {biome}. Valid options are: {biomes}")
+  # Get the available biomes and weathers dictionary
+  biomes_and_weathers = AsciiArt.biomes_and_weathers
+
+  # Check if the selected biome is available
+  if biome not in biomes_and_weathers:
+    raise ValueError(f"Invalid biome type: {biome}. Valid options are: {', '.join(biomes_and_weathers)}")
+  
+  # Adjust weather if it's not compatible with the selected biome
+  if weather not in biomes_and_weathers[biome]:
+    # Change to the first compatible weather
+    weather = next(iter(biomes_and_weathers[biome]), 'day')  # Default to 'day' if no weathers are compatible
+    print(f"Adjusted weather to '{weather}' for biome '{biome}'.") if info >= 3 else None
 
   # Function to filter art by weather
   def filter_art_by_weather_and_biome(art_objects, selected_weather, selected_biome):
