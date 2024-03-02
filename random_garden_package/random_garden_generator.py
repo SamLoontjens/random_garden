6c69565542f3908756f3d45ef96b99389113fa00
SamLoontjens <93867680+SamLoontjens@users.noreply.github.com>
2024-03-02 03:23:03 +0100

    make sure that the art is read as utf-8

--- random_garden_package/random_garden_generator.py
+++ random_garden_package/random_garden_generator.py
@@ -46,7 +46,7 @@ class AsciiArt:
 
   @staticmethod
   def load_from_file(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
       lines = file.readlines()
 
     name = category = source = None
